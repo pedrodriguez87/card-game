6afa0ff7b7834b602bf87bb2519941232f9b218c
Absolutt87 <pedrodriguez87@hotmail.com>
2018-11-30 10:23:48 +0100

    Create users

---
+++ src/main/java/tech/bts/cardgame/controller/UserController.java
@@ -0,0 +1,32 @@
+package tech.bts.cardgame.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import tech.bts.cardgame.model.User;
+import tech.bts.cardgame.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+    private UserRepository userRepository;
+
+    // UserRepository userRepository = new UserRepository();
+    // UserController userCtrl = new UserController(userRepo);
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/users")
+    public void register(@RequestBody User user) {
+
+        System.out.println("Registering user " + user.getName());
+        userRepository.create(user);
+
+    }
+}
