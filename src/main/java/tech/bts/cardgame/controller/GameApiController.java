578a63ba6c2ca1b9dce2f11cb8649fedede66308
Absolutt87 <pedrodriguez87@hotmail.com>
2018-12-03 10:14:26 +0100

    changed name to GameApiController

---
+++ src/main/java/tech/bts/cardgame/controller/GameApiController.java
@@ -0,0 +1,52 @@
+package tech.bts.cardgame.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tech.bts.cardgame.model.Card;
+import tech.bts.cardgame.model.Game;
+import tech.bts.cardgame.service.GameService;
+import tech.bts.cardgame.service.GameUser;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@RestController
+@RequestMapping(path = "/api/games")
+public class GameApiController {
+
+    private GameService gameService;
+
+    @Autowired
+    public GameApiController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @RequestMapping(method = POST)
+    public long createGame() {
+
+        Game game = gameService.createGame();
+        return game.getId();
+    }
+    @RequestMapping(method = GET)
+    public Collection<Game> getAllGames() {
+
+        return gameService.getAllGames();
+
+    }
+
+    @RequestMapping(method = PUT, path = "/{gameId}/join")
+    public void joinGame(@RequestBody GameUser gameUser, @PathVariable("gameId") long gameId) {
+
+        gameUser.setGameId(gameId);
+        gameService.joinGame(gameUser);
+    }
+    @RequestMapping(method = PUT, path ="/{gameId}/pick")
+    public Card pickCard(@RequestBody GameUser gameUser, @PathVariable("gameId") long gameId) {
+
+        gameUser.setGameId(gameId);
+        return gameService.pickCard(gameUser);
+
+    }
+ }
