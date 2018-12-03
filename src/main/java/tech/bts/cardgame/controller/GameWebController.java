638368cd09c02b637cce2afa7f7abd717507ec53
Absolutt87 <pedrodriguez87@hotmail.com>
2018-12-03 10:14:00 +0100

    Simplified GameApiController

---
+++ src/main/java/tech/bts/cardgame/controller/GameWebController.java
@@ -0,0 +1,28 @@
+package tech.bts.cardgame.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import tech.bts.cardgame.model.Game;
+import tech.bts.cardgame.service.GameService;
+
+import java.util.Collection;
+
+@RestController
+public class GameWebController {
+
+    private GameService gameService;
+
+    @Autowired
+    public GameWebController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/games")
+    public Collection<Game> getAllGames() {
+
+        return gameService.getAllGames();
+
+    }
+}
